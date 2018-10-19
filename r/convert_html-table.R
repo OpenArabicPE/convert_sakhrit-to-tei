da88d1c18c7671bd5c3b01f2c4e6905b8bd09da3
Till Grallert <till.grallert@fu-berlin.de>
2018-10-19 20:51:26 +0300

    wrote function to retrieve the table with bibliographic information and save it in various formats

--- r/convert_html-table.R
+++ r/convert_html-table.R
@@ -10,28 +10,36 @@ Sys.setlocale("LC_ALL", "en_US.UTF-8")
 setwd("/BachUni/BachBibliothek/GitHub/OpenArabicPE/convert_sakhrit-to-tei/data/test") #/Volumes/Dessau HD/
 
 # read a html file
-v.Source <- read_html("authorsArticles.aspx?AID=2", encoding = "utf-8") #make sure to specify utf-8 encoding
+v.Url <- "authorsArticles.aspx?AID=2"
+v.Source <- read_html(v.Url, encoding = "utf-8") #make sure to specify utf-8 encoding
+# read all file names in a folder
+v.Filenames <- list.files(pattern="authorsArticles*", full.names=TRUE)
 
-# get the author name
-v.Author.Name <- v.Source %>%
-  html_node(xpath="normalize-space(descendant::node()[@id = 'ContentPlaceHolder1_lbAuthorName'])")
+# function to retrieve the relevant HTML table
+func.Html.Retrieve.Table <- function(v.Url) {
+  v.Source <- read_html(v.Url, encoding = "utf-8") #make sure to specify utf-8 encoding
+  v.Author.Name <- v.Source %>%
+    html_node(xpath="normalize-space(descendant::node()[@id = 'ContentPlaceHolder1_lbAuthorName'])")
+  v.Table <- read_html(v.Url, encoding = "utf-8") %>% #make sure to specify utf-8 encoding
+    html_nodes(xpath="descendant::table[@id = 'ContentPlaceHolder1_gvSearchResult']")
+  v.Df <- v.Table %>% # convert a html table to an R data frame
+    html_table(fill = T) %>%
+    data.frame() %>%
+    # select columns of interest
+    dplyr::select(1,2,3) %>%
+    # rename columns
+    dplyr::rename(
+      article.title = 1,
+      journal.title = 2,
+      journal.issue = 3) %>%
+    # add author name
+    dplyr::mutate(author.name = as.character(v.Author.Name))
+  # save data
+  #save(v.Df, file = paste("../_output/rda/",v.Url,".rda", sep = ""))
+  write.table(v.Df, file = paste("../_output/csv/",v.Url,".csv", sep = "") , row.names = F, quote = T , sep = ",")
+  write_xml(v.Table, file = paste("../_output/html/",v.Url,".html", sep = ""), options = "as_html")
+}
 
-# pull out the bibliographic information
-v.Table <- v.Source %>% 
-  # get a specific node set
-  html_nodes(xpath="descendant::table[@id = 'ContentPlaceHolder1_gvSearchResult']") %>%
-  # convert a html table to an R data frame
-  html_table(fill = T) %>%
-  data.frame() %>%
-  # select columns of interest
-  dplyr::select(1,2,3) %>%
-  # rename columns
-  dplyr::rename(
-    article.title = 1,
-    journal.title = 2,
-    journal.issue = 3) %>%
-  # add author name
-  dplyr::mutate(author.name = as.character(html_node(v.Source, xpath="normalize-space(descendant::node()[@id = 'ContentPlaceHolder1_lbAuthorName'])")))
-
-head(v.Table)
+# apply a function to all files
+sapply(v.Filenames, FUN = func.Html.Retrieve.Table)
 
