3cfe19e767c43c2cbe46bf6a5d7fdbe63fb81a9d
Till Grallert <till.grallert@fu-berlin.de>
2018-10-26 11:40:28 +0300

    added namespaces

--- r/scrape_sakhrit.R
+++ r/scrape_sakhrit.R
@@ -17,32 +17,32 @@ v.Url.Base <- "http://archive.sakhrit.co/"
 #  function retrieve everything from the article details page
 func.Retrieve.ArticlePages.Csv <- function(ArticlePages) {
   # build URL from base and variable
-  v.Url <-paste(v.Url.Base, ArticlePages, sep = "")
+  v.Url <- paste(v.Url.Base, ArticlePages, sep = "")
   # build an If ... then clause that checks whether a URL is actually available
   if (RCurl::url.exists(v.Url)) {
-    v.Source <- read_html(v.Url, encoding = "utf-8") #make sure to specify utf-8 encoding
+    v.Source <- xml2::read_html(v.Url, encoding = "utf-8") #make sure to specify utf-8 encoding
     # PROBLEM: some pages default to "DefaultArchive.aspx"
     # check if the table with bibliographic information is present
     #if (xml_find_first(v.Source, "descendant::td[@class='F_MagazineName']")) {
     # issue level
-    v.Issue <- xml_find_first(v.Source, "descendant::td[@class='F_MagazineName']/table/tr/td[1]")
-    # check if these fields are empty!
-    journal.title <- xml_text(xml_find_first(v.Issue, "child::a[1]"))
-    journal.issue <- xml_text(xml_find_first(v.Issue, "child::a[2]"))
-    journal.issue.url <- xml_attr(xml_find_first(v.Issue, "child::a[2]"), attr = "href")
-    date.publication <- xml_text(xml_find_first(v.Issue, "child::span[1]"))
-    place.publication <- xml_text(xml_find_first(v.Source, "descendant::a[@class='countrylable']"))
+    v.Issue <- xml2::xml_find_first(v.Source, "descendant::td[@class='F_MagazineName']/table/tr/td[1]")
+    # check if these fields are empty! And provide NA
+    journal.title <- xml2::xml_text(xml2::xml_find_first(v.Issue, "child::a[1]"))
+    journal.issue <- xml2::xml_text(xml2::xml_find_first(v.Issue, "child::a[2]"))
+    journal.issue.url <- xml2::xml_attr(xml2::xml_find_first(v.Issue, "child::a[2]"), attr = "href")
+    date.publication <- xml2::xml_text(xml2::xml_find_first(v.Issue, "child::span[1]"))
+    place.publication <- xml2::xml_text(xml2::xml_find_first(v.Source, "descendant::a[@class='countrylable']"))
     # article level
-    v.Author <- xml_find_all(v.Source, "descendant::a[child::span[@id='ContentPlaceHolder1_Label2']]")
-    author.name <- xml_text(xml_find_first(v.Author,"span[@id='ContentPlaceHolder1_Label2']"))
-    author.url <- xml_attr(v.Author, attr = "href")
+    v.Author <- xml2::xml_find_all(v.Source, "descendant::a[child::span[@id='ContentPlaceHolder1_Label2']]")
+    author.name <- xml2::xml_text(xml2::xml_find_first(v.Author,"span[@id='ContentPlaceHolder1_Label2']"))
+    author.url <- xml2::xml_attr(v.Author, attr = "href")
     article.url <- ArticlePages
     # links to images
-    v.Pages <- xml_find_all(v.Source, "descendant::div[@id='svPlayerId']/div/div/div/img[@class='slide_image']")
-    facsimile.url <- xml_attr(v.Pages, attr = "src")
+    v.Pages <- xml2::xml_find_all(v.Source, "descendant::div[@id='svPlayerId']/div/div/div/img[@class='slide_image']")
+    facsimile.url <- xml2::xml_attr(v.Pages, attr = "src")
     # still missing: article.title 
     # construct data frame
-    v.Df <- data_frame(
+    v.Df <- dplyr::data_frame(
       journal.title, journal.issue, journal.issue.url, date.publication, place.publication,
       author.name, author.url,
       article.url, 
@@ -76,11 +76,12 @@ v.Urls <- unlist(v.Urls)
 # set working directory
 
 # apply a function to all files
-sapply(v.Urls, FUN = func.Retrieve.ArticlePages.Csv)
+lapply(v.Urls, FUN = func.Retrieve.ArticlePages.Csv)
 
 # debugging
+library(purrr)
 
-v.Url <-paste(v.Url.Base, "ArticlePages.aspx?ArticleID=241", sep = "")
+v.Url <-paste(v.Url.Base, "ArticlePages.aspx?ArticleID=240", sep = "")
 v.Source <- read_html(v.Url, encoding = "utf-8")
 v.Issue <- xml_find_first(v.Source, "descendant::td[@class='F_MagazineName']")
 journal.title <- xml_text(xml_find_first(v.Issue, "child::a[1]"))
