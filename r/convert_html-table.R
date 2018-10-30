a8ecd5d3f27b76a61db3f8c74384a0ad3524d3a8
Till Grallert <till.grallert@fu-berlin.de>
2018-10-30 15:09:48 +0200

    added function to slice data into rolling periods

--- r/convert_html-table.R
+++ r/convert_html-table.R
@@ -18,31 +18,33 @@ setwd("/BachUni/BachBibliothek/GitHub/OpenArabicPE/convert_sakhrit-to-tei/data/t
 # apply functions to the full data set
 # navigate to folder containing a scraped copy of the website
 setwd("/BachUni/BachBibliothek/GitHub/OpenArabicPE/data_sakhrit/sakhrit")
+setwd("/BachUni/programming/wget/dumpsite/sakhrit/contents")
 
-# read all file names in a folder
+# read all file names in a folder containing the scraped HTML from Sakhrit
 v.Filenames.Authors <- list.files(pattern="authorsArticles*", full.names=TRUE)
 v.Filenames.Articles <- list.files(pattern="ArticlePages*", full.names=TRUE)
 v.Filenames.Contents <- list.files(pattern="contents*", full.names=TRUE)
 
-# apply a function to all files
-sapply(v.Filenames.Contents, FUN = f.Retrieve.contents.Csv)
-sapply(v.Filenames.Authors, FUN = f.Retrieve.authorsArticles.Csv)
-sapply(v.Filenames.Authors, FUN = f.Retrieve.authorsArticles.Html)
-sapply(v.Filenames.Articles, FUN = f.Retrieve.ArticlePages.Csv)
+# apply a function to all files: retrieve information from HTML pages and save results as CSV
+sapply(v.Filenames.Contents, FUN = f.retrieve.contents.csv)
+sapply(v.Filenames.Authors, FUN = f.retrieve.authorsArticles.csv)
+sapply(v.Filenames.Authors, FUN = f.retrieve.authorsArticles.html)
+sapply(v.Filenames.Articles, FUN = f.retrieve.ArticlePages.csv)
 
 # combine the individual result files into single data frames
 setwd("/BachUni/BachBibliothek/GitHub/OpenArabicPE/data_sakhrit/csv")
+#setwd("/BachUni/BachBibliothek/GitHub/OpenArabicPE/convert_sakhrit-to-tei/data/test/csv")
 v.Filenames.Authors <- list.files(pattern="authorsArticles*", full.names=TRUE)
 v.Filenames.Articles <- list.files(pattern="ArticlePages*", full.names=TRUE)
 v.Filenames.Contents <- list.files(pattern="contents*", full.names=TRUE)
 
 # Purrr:: read all files and combine them into one dataframe
-data.Sakhrit.authorsArticles <- purrr::map_df(v.Filenames.Authors, read.csv)
-data.Sakhrit.Articles <- purrr::map_df(v.Filenames.Articles, read.csv)
-data.Sakhrit.Contents <- purrr::map_df(v.Filenames.Contents, read.csv)
+data.sakhrit.authorsArticles <- purrr::map_df(v.Filenames.Authors, read.csv)
+data.sakhrit.articles <- purrr::map_df(v.Filenames.Articles, read.csv)
+data.sakhrit.contents <- purrr::map_df(v.Filenames.Contents, read.csv)
 
 # do some data cleaning:
-data.Sakhrit.authorsArticles <- data.Sakhrit.authorsArticles %>%
+data.sakhrit.authorsArticles <- data.sakhrit.authorsArticles %>%
   # 1. rename columns
   dplyr::rename(
     article.title = article.title.1.35.,
@@ -54,15 +56,49 @@ data.Sakhrit.authorsArticles <- data.Sakhrit.authorsArticles %>%
   # 2. remove all rows that contain NA values or numbers for article title
   tidyr::drop_na(article.url)
 
+# fix data types: dates
+data.sakhrit.contents$date.publication.iso <- anydate(data.sakhrit.contents$date.publication.iso)
+
 # write results to file
-write.table(data.Sakhrit.authorsArticles, file = "../_output/csv/authorsArticles_all.csv", row.names = F, quote = T, sep = ",")
-save(data.Sakhrit.authorsArticles, file = "../output/rda/authorsArticles_all.rda")
-write.table(data.Sakhrit.Contents, file = "../_output/csv/contents_all.csv", row.names = F, quote = T, sep = ",")
-save(data.Sakhrit.Contents, file = "../output/rda/contents_all.rda")
+setwd("/BachUni/BachBibliothek/GitHub/OpenArabicPE/data_sakhrit/_output")
+write.table(data.sakhrit.authorsArticles, file = "csv/authorsArticles_all.csv", row.names = F, quote = T, sep = ",")
+save(data.sakhrit.authorsArticles, file = "rda/authorsArticles_all.rda")
+
+write.table(data.sakhrit.contents, file = "csv/contents_all.csv", row.names = F, quote = T, sep = ",")
+save(data.sakhrit.contents, file = "rda/contents_all.rda")
+
+# filter for specific periods
+data.sakhrit.contents.1870to1920 <- f.date.publication.period(data.sakhrit.contents, "1870-01-01", "1920-12-31")
+write.table(data.sakhrit.contents.1870to1920, file = "csv/contents_1870-1920.csv", row.names = F, quote = T, sep = ",")
+save(data.sakhrit.contents.1870to1920, file = "rda/contents_1870-1920.rda")
+
+# slice data into rolling decades
+## load data
+setwd("/BachUni/BachBibliothek/GitHub/OpenArabicPE/data_sakhrit/")
+load("rda/contents_all.rda")
+## 1. define start year
+## 2. define a stop year
+## 3. select data that falls between the two
+## 4. increment start year by one and repeat until start year equals stop year
+
+f.date.slice.decade <- function(df.input, date.start, date.stop) {
+  date.decade <- date.start + 9
+  #df.input <- data.sakhrit.contents
+  if(date.start <= date.stop) {
+    # limit df.input to current decade
+    v.Df <- df.input[lubridate::year(df.input$date.publication.iso) >= date.start & lubridate::year(df.input$date.publication.iso) <= date.decade,]
+    # save output
+    write.table(v.Df, file = paste("csv/contents_",date.start ,"-", date.decade, ".csv", sep = ""), row.names = F, quote = T, sep = ",")
+    save(v.Df, file = paste("rda/contents_",date.start ,"-", date.decade, ".rda", sep = ""))
+    f.date.slice.decade(df.input, date.start + 1, date.stop)
+  }
+}
+
+f.date.slice.decade(data.sakhrit.contents, 1921, 2008)
 
 
 # convert to and save as XML
-v.Data <- data.Sakhrit.authorsArticles
+v.Data <- data.sakhrit.authorsArticles
 v.Xml <- xmlTree()
 v.Xml$addTag("TEI", close = F)
 v.Xml$addTag("teiHeader", close = T)
